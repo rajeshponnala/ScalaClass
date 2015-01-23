b2a823c452c53c562d318456a9b95e9d68dc3118
Tabrez Iqbal <tabrez.iqbal@gmail.com>
2015-01-23 19:46:23 +0530

    add spaces for code readability; remove unnecessary import statement

--- src/main/scala/com/example/loopsrecursion.scala
+++ src/main/scala/com/example/loopsrecursion.scala
@@ -1,28 +1,30 @@
 package com.example
-import collection.immutable.List
 
-object loopsrecursion {
+object LoopsRecursion {
 
-  def NDots(n: Int)=for(e <- 1 to n)println(".")
+  def nDots(n: Int) = for(e <- 1 to n) print(".")
 
-  def power(x: Int,y: Int): Int=
+  def power(x: Int,y: Int): Int =
   {
-    var acc=1
+    var acc = 1
     for(e <- 1 to y)
-      acc=acc*x
+      acc = acc * x
     acc
   }
 
-  def sumupto(n: Int): Int=
+  def sumUpto(n: Int): Int =
   {
-    def loop(n1: Int,acc: Int): Int=
-      if(n1==0)acc else loop(n1-1,acc+n1)
-    loop(n,0)
+    def loop(n1: Int, acc: Int): Int =
+      if(n1 == 0) acc 
+      else loop(n1 - 1, acc + n1)
+    loop(n, 0)
   }
 
-  def reverse[T](l: List[T]): List[T]={
-     def loop(l1: List[T],acc :List[T]): List[T]=
-       if(l1.isEmpty) acc else loop(l1.tail,l1.head::acc)
-    loop(l,List.empty)
+  def reverse[T](l: List[T]): List[T] = 
+  {
+     def loop(l1: List[T], acc :List[T]): List[T] =
+       if(l1.isEmpty) acc 
+       else loop(l1.tail, l1.head::acc)
+    loop(l, List.empty)
   }
 }
