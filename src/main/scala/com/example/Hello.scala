8600a91ff6d8180eae3c41ec059c9251b047226f
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-01-24 15:09:06 +0530

    add minmax function execution

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,11 +2,14 @@ package com.example
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val lr=loopsrecursion
-    lr.NDots(6)
+    val lr=LoopsRecursion
+    val minmax=Collections.minmax(List(3,10,6,8,7,12,90))
+    lr.nDots(6)
     println(lr.power(2,10))
-    println(lr.sumupto(10))
+    println(lr.sumUpto(10))
     lr.reverse(List("a","n","n","i","h","c")).foreach(println)
+    println("min : " + minmax._1)
+    println("max : " + minmax._2)
 
-  }
+}
 }
