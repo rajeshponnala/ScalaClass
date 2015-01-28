10226413b78e1e7fbd5fa2bd60114ddafc629a1a
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-01-28 19:27:12 +0530

    implement removeIf function

--- src/main/scala/com/example/Functions.scala
+++ src/main/scala/com/example/Functions.scala
@@ -1,6 +1,14 @@
 package com.example
-import collection.immutable.List
+import collection.mutable.ListBuffer
 
 object Functions {
-  
+
+  def removeIf(fn: Int => Boolean,l: List[Int]): ListBuffer[Int]= {
+
+    var res=ListBuffer.empty[Int]
+    for(e <- l)
+      if(!fn(e))
+        res+=e
+    res
+   }
 }
